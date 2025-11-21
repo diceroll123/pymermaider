6e303fd5a2d96b5f33fbbc812c2c38020f461e12
Steve C <diceroll123@gmail.com>
2025-11-21 18:03:53 -0500

    Make basePath conditional on NODE_ENV

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/pymermaider",
+  basePath: process.env.NODE_ENV === "production" ? "/pymermaider" : "",
   images: {
     unoptimized: true,
   },
